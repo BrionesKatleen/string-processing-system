a4eeeaa0320154646fe580c4ea7c993c5310d1f9
Tamago! <brioneskatleen58@gmail.com>
2026-02-28 12:28:42 +0800

    Add initial Program.cs for String Processing System
    Initial implementation of the String Processing System.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+/*
+    NAME: Briones, Katleen L.
+    DATE: Feb 28 2026
+    ACTIVITY 2: String Processing System
+*/
+namespace StringSystem.View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringView view = new StringView();
+            view.Run();
+        }
+    }
+}
